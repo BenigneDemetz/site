923fbff64097aef35bdbb0b4aa207e086050e5d4
BenigneDemetz <72600691+BenigneDemetz@users.noreply.github.com>
2022-05-16 16:32:41 +0200

    github link navbar

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
             $_SESSION['pseudo'] = $pseudo;
             $_SESSION['role'] = $json_a[$pseudo]['role'];
 
-        }
-        else {
+        } else {
             echo "pas connecté";
         }
     }
@@ -33,12 +32,11 @@
         }
 
 
-
     </script>
     <!--<link rel="stylesheet" href="css.css">-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap.css">
-<!--    <link rel="icon" type="image/png" href="logo.png" />-->
+    <!--    <link rel="icon" type="image/png" href="logo.png" />-->
     <style>
         :root {
             --bg-color: #060615;
@@ -118,9 +116,44 @@
 
         .navbar {
             width: available;
-            height: 3.5em;
+            height: 56px;
             background: var(--navbar-color);
         }
+
+        #dotborder {
+            position: absolute;
+            top: 3px;
+            right: 9px;
+            width: 50px;
+            height: 50px;
+            -webkit-border-radius: 25px;
+            -moz-border-radius: 25px;
+            border-radius: 25px;
+            background: white;
+        }
+
+        #dotbg {
+            position: absolute;
+            top: 4px;
+            right: 10px;
+            width: 48px;
+            height: 48px;
+            -webkit-border-radius: 25px;
+            -moz-border-radius: 25px;
+            border-radius: 25px;
+            background: var(--navbar-color);
+        }
+
+        #githubnavbar {
+            position: absolute;
+            top: 5px;
+            right: 11px;
+            width: 46px;
+            height: 46px;
+            z-index: 1;
+
+        }
+
 
     </style>
 </head>
@@ -131,139 +164,149 @@
         if (isset($_SESSION['pseudo'])) {
             if ($_SESSION['pseudo'] != null) {
                 echo $_SESSION['pseudo'] . ' - ' . $_SESSION['role'];
-        }
+            }
         }
         ?></label>
+    <a href="https://github.com/BenigneDemetz/site" target="_blank">
+        <span id="dotborder"></span>
+        <span id="dotbg"></span>
+        <img src="githubviolet.png" id="githubnavbar">
+    </a>
     <?php
     if (isset($_SESSION['pseudo'])) {
-    if ($_SESSION['pseudo'] != null) {
-        $string = file_get_contents("profiles.json");
-        $json_a = json_decode($string, true);
-        echo '
-                    <form action="index.php" method="post" class="searchandupload" style="position: absolute; right: 1em">
-                        <input type="submit" value="Se déconnecter" style="background: linear-gradient(' . random_int(0,360) . 'deg, var(--button-color1), var(--button-color2));" name="deco">
+        if ($_SESSION['pseudo'] != null) {
+            $string = file_get_contents("profiles.json");
+            $json_a = json_decode($string, true);
+            echo '
+                    <form action="index.php" method="post" class="searchandupload" style="position: absolute; right: 5em">
+                        <input type="submit" value="Se déconnecter" style="background: linear-gradient(' . random_int(0, 360) . 'deg, var(--button-color1), var(--button-color2));" name="deco">
                     </form>';
-    }}
+        }
+    }
     ?>
 </div>
-<div style="margin: 1em 1em 1em 1em;">
-<div class="searchandupload">
 
-    <div style="display: flex; flex-direction: row; padding-bottom: 12px; border-bottom: rgba(145,145,145,0.5);
+
+<div style="margin: 1em 1em 1em 1em;">
+    <div class="searchandupload">
+
+        <div style="display: flex; flex-direction: row; padding-bottom: 12px; border-bottom: rgba(145,145,145,0.5);
     border-bottom-width: 1px;border-bottom-style: double; height: fit-content; width: 100%">
-        <form action="index.php" method="get" class="searchandupload" style="">
-            <span>Rechercher :</span>
-            <div class="inputtext"><input type="text" name="id"></div>
-            <input type="submit" value="Chercher" style="background: linear-gradient(<?php echo random_int(0,360)?>deg, var(--button-color1), var(--button-color2));">
-        </form>
-        <?php
-        if (!isset($_SESSION['pseudo']) or $_SESSION['pseudo'] == null) {
+            <form action="index.php" method="get" class="searchandupload" style="">
+                <span>Rechercher :</span>
+                <div class="inputtext"><input type="text" name="id"></div>
+                <input type="submit" value="Chercher"
+                       style="background: linear-gradient(<?php echo random_int(0, 360) ?>deg, var(--button-color1), var(--button-color2));">
+            </form>
+            <?php
+            if (!isset($_SESSION['pseudo']) or $_SESSION['pseudo'] == null) {
                 echo '
         <form action="index.php" method="post" class="searchandupload" style="position: absolute; right: 1em">
             <span>Se connecter :</span>
             <div class="inputtext"><input type="text" name="pseudo" placeholder="sabri"></div>
-            <input type="submit" value="connect" style="background: linear-gradient(' . random_int(0,360) . 'deg, var(--button-color1), var(--button-color2));">
+            <input type="submit" value="connect" style="background: linear-gradient(' . random_int(0, 360) . 'deg, var(--button-color1), var(--button-color2));">
         </form>';
             }
-        ?>
+            ?>
+        </div>
+
+
+        <form action="upload.php" method="post" enctype="multipart/form-data" class="searchandupload" style="padding-top: 6px; padding-bottom: 12px; border-bottom: rgba(145,145,145,0.5);
+          border-bottom-width: 1px;border-bottom-style: double;">
+
+
+            <div class="file-input">
+                <input type="file" name="filetoup" id="file" class="file" required onchange="updateFile(event)">
+                <label for="file">Select file</label>
+                <span class="text-upload">Nom du fichier :</span>
+                <input type="text" name="name" class="custom-file-input" id="filename" required>
+            </div>
+
+
+            <input type="submit" value="Upload fichier" name="submit"
+                   style="background: linear-gradient(<?php echo random_int(0, 360) ?>deg, var(--button-color1), var(--button-color2));">
+
+        </form>
+        <form action="index.php" method="post" class="searchandupload" style="padding-top: 6px; padding-bottom: 12px; border-bottom: rgba(145,145,145,0.5);
+          border-bottom-width: 1px;border-bottom-style: double;">
+
+            <div style="display: flex; flex-direction: column; gap: 4px">
+                <div>
+                    <span style="padding-right: 4px">Lien :</span>
+                    <input type="text" name="link" required="">
+                </div>
+                <div>
+                    <span style="padding-right: 4px">Nom :</span>
+                    <input type="text" name="name" required="">
+                </div>
+            </div>
+
+            <input type="submit" value="Upload lien" name="submit"
+                   style="background: linear-gradient(<?php echo random_int(0, 360) ?>deg, var(--button-color1), var(--button-color2));">
+
+        </form>
+        <br>
     </div>
 
-
-    <form action="upload.php" method="post" enctype="multipart/form-data" class="searchandupload" style="padding-top: 6px; padding-bottom: 12px; border-bottom: rgba(145,145,145,0.5);
-          border-bottom-width: 1px;border-bottom-style: double;">
-
-
-        <div class="file-input">
-            <input type="file" name="filetoup" id="file" class="file" required onchange="updateFile(event)">
-            <label for="file">Select file</label>
-            <span class ="text-upload" >Nom du fichier :</span>
-            <input type="text" name="name" class="custom-file-input" id="filename" required>
-        </div>
+    <div id="fileslist">
+        <?php
 
 
-        <input type="submit" value="Upload fichier" name="submit" style="background: linear-gradient(<?php echo random_int(0,360)?>deg, var(--button-color1), var(--button-color2));">
+        if (isset($_GET['id'])) {
 
-    </form>
-    <form action="index.php" method="post" class="searchandupload" style="padding-top: 6px; padding-bottom: 12px; border-bottom: rgba(145,145,145,0.5);
-          border-bottom-width: 1px;border-bottom-style: double;">
-
-        <div style="display: flex; flex-direction: column; gap: 4px">
-            <div>
-                <span style="padding-right: 4px">Lien :</span>
-                <input type="text" name="link" required="">
-            </div>
-            <div>
-                <span style="padding-right: 4px">Nom :</span>
-                <input type="text" name="name" required="">
-            </div>
-        </div>
-
-        <input type="submit" value="Upload lien" name="submit" style="background: linear-gradient(<?php echo random_int(0,360)?>deg, var(--button-color1), var(--button-color2));">
-
-    </form>
-    <br>
-</div>
-
-<div id="fileslist">
-    <?php
-
-
-    if (isset($_GET['id'])) {
-
-        if ($_GET['id'] == 'timbres') {
-            echo '<a href="timbresb.pdf" download>timbres</a>';
-        }
-
-        if ($_GET['id'] == 'film') {
-            echo ' <meta http-equiv="refresh" content="0; URL=helo/film" />';
-        }
-
-        $string = file_get_contents("liens.json");
-        $json_a = json_decode($string);
-        foreach ($json_a as $key => $value) {
-            if ($_GET['id'] == $key) {
-                echo ' <meta http-equiv="refresh" content="0; URL=' . $value . '" />';
+            if ($_GET['id'] == 'timbres') {
+                echo '<a href="timbresb.pdf" download>timbres</a>';
             }
+
+            if ($_GET['id'] == 'film') {
+                echo ' <meta http-equiv="refresh" content="0; URL=helo/film" />';
+            }
+
+            $string = file_get_contents("liens.json");
+            $json_a = json_decode($string);
+            foreach ($json_a as $key => $value) {
+                if ($_GET['id'] == $key) {
+                    echo ' <meta http-equiv="refresh" content="0; URL=' . $value . '" />';
+                }
+            }
+
         }
 
-    }
 
+        if (isset($_POST['link'])) {
 
-    if (isset($_POST['link'])) {
+            $link = $_POST['link'];
+            $name = $_POST['name'];
 
-        $link = $_POST['link'];
-        $name = $_POST['name'];
+            $string = file_get_contents("liens.json");
+            $json_a = json_decode($string, true);
+            $json_a[$name] = $link;
+            $fp = fopen('liens.json', 'w');
+            fwrite($fp, json_encode($json_a));
+            fclose($fp);
 
-        $string = file_get_contents("liens.json");
-        $json_a = json_decode($string, true);
-        $json_a[$name] = $link;
-        $fp = fopen('liens.json', 'w');
-        fwrite($fp, json_encode($json_a));
-        fclose($fp);
+        }
 
-    }
-
-    if (isset($_SESSION['pseudo'])) {
-        if ($_SESSION['pseudo'] != null) {
-            if ($_SESSION['pseudo']) {
-                echo "Je n'ai commencé à faire que site qu'il n'y a qu'une ou deux semaine pour partager des fichiers avec des amis.
+        if (isset($_SESSION['pseudo'])) {
+            if ($_SESSION['pseudo'] != null) {
+                if ($_SESSION['pseudo']) {
+                    echo "Je n'ai commencé à faire que site qu'il n'y a qu'une ou deux semaine pour partager des fichiers avec des amis.
                 Il me permet juste de vous montrer que j'aime développer lorsque j'ai un but derriere et pas developper betement comme à l'iut.
                 Je continue de le modifier au fil du temps. Je n'ai fais le formulaire de connection que ce week-end.";
+                }
+            }
+        } else {
+            $files = scandir('./uploads/');
+            foreach ($files as $file) {
+                if ($file == "." or $file == "..") {
+                    continue;
+                }
+                echo '<a href=uploads/' . $file . ' target=”_blank” style="overflow: hidden; text-overflow-start: clip; text-overflow-middle: ellipsis; text-overflow-end: clip; text-overflow-min-width: 0 3ch;">' . $file . '</a>';
             }
         }
-    }
-    else {
-        $files = scandir('./uploads/');
-        foreach ($files as $file) {
-            if ($file == "." or $file == "..") {
-                continue;
-            }
-            echo '<a href=uploads/' . $file . ' target=”_blank” style="overflow: hidden; text-overflow-start: clip; text-overflow-middle: ellipsis; text-overflow-end: clip; text-overflow-min-width: 0 3ch;">' . $file . '</a>';
-        }
-    }
 
-    ?>
-</div>
+        ?>
+    </div>
 </div>
 </body>
 </html>
